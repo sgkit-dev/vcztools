Fix off-by-one error for VCF regions

vcztools/regions.py
@@ -18,7 +18,7 @@ def parse_targets_string(targets: str) -> tuple[str, Optional[int], Optional[int
         return contig, None, None
 
 
-def pslice_to_slice(
+def region_to_slice(
     all_contigs: List[str],
     variant_contig: Any,
     variant_position: Any,
@@ -26,6 +26,12 @@ def pslice_to_slice(
     start: Optional[int] = None,
     end: Optional[int] = None,
 ) -> slice:
+    """Convert a VCF region to a Python slice."""
+
+    # subtract one from start since VCF is 1-based (fully closed),
+    # but Python slices are 0-based (half open)
+    start = None if start is None else start - 1
+
     contig_index = all_contigs.index(contig)
     contig_range = np.searchsorted(variant_contig, [contig_index, contig_index + 1])
 
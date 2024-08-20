fc935739c52537cf5a61bf8590a35ae6c0f165e4
willtyler <will.tyler11@gmail.com>
2024-08-20 19:41:00 +0000

    Add a query format parser

--- tests/test_query.py
+++ tests/test_query.py
@@ -1,8 +1,11 @@
 import pathlib
 from io import StringIO
 
+import pyparsing as pp
+import pytest
+
 from tests.utils import vcz_path_cache
-from vcztools.query import list_samples
+from vcztools.query import QueryFormatParser, list_samples
 
 
 def test_list_samples(tmp_path):
@@ -13,3 +16,49 @@ def test_list_samples(tmp_path):
     with StringIO() as output:
         list_samples(vcz_path, output)
         assert output.getvalue() == expected_output
+
+
+class TestQueryFormatParser:
+    @pytest.fixture()
+    def parser(self):
+        return QueryFormatParser()
+
+    @pytest.mark.parametrize(
+        ("expression", "expected_result"),
+        [
+            ("%CHROM", ["%CHROM"]),
+            (r"\n", ["\n"]),
+            (r"\t", ["\t"]),
+            (r"%CHROM\n", ["%CHROM", "\n"]),
+            ("%CHROM  %POS  %REF", ["%CHROM", "  ", "%POS", "  ", "%REF"]),
+            (r"%CHROM  %POS0  %REF\n", ["%CHROM", "  ", "%POS0", "  ", "%REF", "\n"]),
+            (
+                r"%CHROM\t%POS\t%REF\t%ALT{0}\n",
+                ["%CHROM", "\t", "%POS", "\t", "%REF", "\t", ["%ALT", 0], "\n"],
+            ),
+            (
+                r"%CHROM\t%POS0\t%END\t%ID\n",
+                ["%CHROM", "\t", "%POS0", "\t", "%END", "\t", "%ID", "\n"],
+            ),
+            (r"%CHROM:%POS\n", ["%CHROM", ":", "%POS", "\n"]),
+            (r"%AC{1}\n", [["%AC", 1], "\n"]),
+            (
+                r"Allelic depth: %INFO/AD\n",
+                ["Allelic", " ", "depth:", " ", "%INFO/AD", "\n"],
+            ),
+        ],
+    )
+    def test_valid_expressions(self, parser, expression, expected_result):
+        assert parser(expression).as_list() == expected_result
+
+    @pytest.mark.parametrize(
+        "expression",
+        [
+            ("%ac",),
+            ("%AC {1}",),
+            ("% CHROM",),
+        ],
+    )
+    def test_invalid_expressions(self, parser, expression):
+        with pytest.raises(pp.ParseException):
+            parser(expression)

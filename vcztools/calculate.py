d1214ba4227208118caa0677b56caef03d0a2ab7
Tom White <tom.e.white@gmail.com>
2025-05-27 12:18:34 +0100

    Support TYPE field (ref and snp)

---
+++ vcztools/calculate.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+# Variant types
+REF = -1  # missing value
+SNP = 1 << 0
+UNCLASSIFIED = 1 << 8
+
+
+def get_variant_type(ref: str, alt: str) -> int:
+    """Return the variant type int for the given REF, ALT combination."""
+    if len(alt) == 0:
+        return REF
+    elif len(ref) == 1 and len(alt) == 1 and alt != "*":
+        if ref == alt:
+            return REF
+        else:
+            return SNP
+    elif alt == "<*>" or alt == "<NON_REF>":
+        return REF
+    else:
+        return UNCLASSIFIED
+
+
+def calculate_variant_type(variant_allele: np.ndarray) -> np.ndarray:
+    """Calculate the variant type array from the variant_allele array."""
+    ref = variant_allele[:, 0]
+    alt = variant_allele[:, 1:]
+
+    variant_type = np.zeros(alt.shape, dtype=np.int16)
+
+    for i in range(alt.shape[0]):
+        for j in range(alt.shape[1]):
+            variant_type[i, j] = get_variant_type(ref[i], alt[i, j])
+    return variant_type

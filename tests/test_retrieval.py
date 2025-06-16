efb3d691b58c599431adbbaa7c92b59d3e5acb6e
Tom White <tom.e.white@gmail.com>
2025-06-16 09:59:17 +0100

    Add API for iterating over variants

--- tests/test_retrieval.py
+++ tests/test_retrieval.py
@@ -1,9 +1,10 @@
 import pathlib
 
 import numpy.testing as nt
+import pytest
 import zarr
 
-from vcztools.retrieval import variant_chunk_iter
+from vcztools.retrieval import variant_chunk_iter, variant_iter
 from vcztools.samples import parse_samples
 
 from .utils import vcz_path_cache
@@ -32,3 +33,33 @@ def test_variant_chunk_iter():
     # are False (NA00002 and NA00003), since sample NA00001 matched filter criteria,
     # but was then removed by samples_selection
     nt.assert_array_equal(chunk_data["call_mask"], [[True, False], [False, False]])
+
+
+def test_variant_iter():
+    original = pathlib.Path("tests/data/vcf") / "sample.vcf.gz"
+    vcz = vcz_path_cache(original)
+
+    iter = variant_iter(
+        vcz,
+        fields=["variant_contig", "variant_position", "call_DP", "call_GQ"],
+        regions="20:1230236-",
+        include="FMT/DP>3",
+        samples="NA00002,NA00003",
+    )
+
+    variant1 = next(iter)
+    assert variant1["variant_contig"] == 1
+    assert variant1["variant_position"] == 1230237
+    nt.assert_array_equal(variant1["call_DP"], [4, 2])
+    nt.assert_array_equal(variant1["call_GQ"], [48, 61])
+    nt.assert_array_equal(variant1["call_mask"], [True, False])
+
+    variant2 = next(iter)
+    assert variant2["variant_contig"] == 1
+    assert variant2["variant_position"] == 1234567
+    nt.assert_array_equal(variant2["call_DP"], [2, 3])
+    nt.assert_array_equal(variant2["call_GQ"], [17, 40])
+    nt.assert_array_equal(variant2["call_mask"], [False, False])
+
+    with pytest.raises(StopIteration):
+        next(iter)

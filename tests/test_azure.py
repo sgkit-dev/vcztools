367ce265e3bafc244d0b712293cfeb5c53fda66e
Tom White <tom.e.white@gmail.com>
2026-04-24 16:06:31 +0100

    Reorg files

--- tests/test_azure.py
+++ tests/test_azure.py
@@ -4,16 +4,15 @@ import subprocess
 import time
 import uuid
 
+import icechunk
 import numpy as np
 import pytest
 import zarr
+from icechunk import Repository
 
-from vcztools.icechunk import make_icechunk_storage
+from vcztools.utils import make_icechunk_storage
 
 from .utils import copy_store_to_icechunk, to_vcz_icechunk
-
-icechunk = pytest.importorskip("icechunk")
-from icechunk import Repository  # noqa E402
 
 AZURITE_ACCOUNT = "devstoreaccount1"
 AZURITE_KEY = (
@@ -129,3 +128,81 @@ def test_copy_then_append_azure_icechunk(tmp_path, fx_sample_vcz3, azurite_env):
 
     assert actual_root["sample_id"][:].tolist() == ["NA00001", "NA00002", "NA00003"]
     _assert_roots_equal(expected_root, actual_root)
+
+
+@pytest.mark.parametrize(
+    ("url", "expected"),
+    [
+        pytest.param(
+            "az://myaccount/my-container/some/prefix/store.vcz",
+            {
+                "account": "myaccount",
+                "container": "my-container",
+                "prefix": "some/prefix/store.vcz",
+                "from_env": True,
+            },
+            id="az",
+        ),
+        pytest.param(
+            "azure://myaccount/my-container/some/prefix/store.vcz",
+            {
+                "account": "myaccount",
+                "container": "my-container",
+                "prefix": "some/prefix/store.vcz",
+                "from_env": True,
+            },
+            id="azure",
+        ),
+        pytest.param(
+            "abfs://my-container@myaccount.dfs.core.windows.net/some/prefix/store.vcz",
+            {
+                "account": "myaccount",
+                "container": "my-container",
+                "prefix": "some/prefix/store.vcz",
+                "from_env": True,
+            },
+            id="abfs",
+        ),
+        pytest.param(
+            "abfss://my-container@myaccount.dfs.core.windows.net/some/prefix/store.vcz",
+            {
+                "account": "myaccount",
+                "container": "my-container",
+                "prefix": "some/prefix/store.vcz",
+                "from_env": True,
+            },
+            id="abfss",
+        ),
+        pytest.param(
+            "https://myaccount.blob.core.windows.net/my-container/some/prefix/store.vcz",
+            {
+                "account": "myaccount",
+                "container": "my-container",
+                "prefix": "some/prefix/store.vcz",
+                "from_env": True,
+            },
+            id="https-blob",
+        ),
+        pytest.param(
+            "https://myaccount.dfs.core.windows.net/my-container/some/prefix/store.vcz",
+            {
+                "account": "myaccount",
+                "container": "my-container",
+                "prefix": "some/prefix/store.vcz",
+                "from_env": True,
+            },
+            id="https-dfs",
+        ),
+    ],
+)
+def test_make_icechunk_storage_azure_urls(monkeypatch, url, expected):
+    captured = {}
+
+    def fake_azure_storage(**kwargs):
+        captured.update(kwargs)
+        return "azure-store"
+
+    monkeypatch.setattr(icechunk, "azure_storage", fake_azure_storage)
+
+    assert make_icechunk_storage(url) == "azure-store"
+    assert captured == expected
